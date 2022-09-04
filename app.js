a8b009c3b457e2af80d88ea41a7930cbfbdbf12d
Sushmita <sushmitakumari363@gmail.com>
2022-09-04 09:30:54 +0530

    date to string function added

--- app.js
+++ app.js
@@ -10,4 +10,24 @@ function checkPalindrome(str){
     return str===reverse;
 }
 
+function convertDateToString(date){
+    var dateStr={day:"",month:"",year:""};
+    if(date.day<10){
+        dateStr.day='0'+date.day;
+    }
+    else
+    {
+        dateStr.day=date.day.toString();
+    }
+    if(date.month<10){
+        dateStr.month='0'+date.month;
+    }
+    else
+    {
+        dateStr.month=date.month.toString();
+    }
+    dateStr.year=date.year.toString();
+    console.log(dateStr);
+    return dateStr;
 
+}
