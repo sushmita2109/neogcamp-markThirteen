590bbf1699130695545109ecb8bc0230e38e1575
Sushmita <sushmitakumari363@gmail.com>
2022-09-04 09:05:36 +0530

    check palindrome func added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 function reverseString(str){
     var charList=str.split("");
-    console.log(charList);
     var reverseChar=charList.reverse();
-    console.log(reverseChar)
     var reverseString=reverseChar.join('');
-    console.log(reverseString)
     return reverseString;
 }
 
-console.log(reverseString('hello'))
+function checkPalindrome(str){
+    var reverse=reverseString(str);
+    return str===reverse;
+}
+
+
